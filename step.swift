090ac1e21fcd01b3067719eec3c6298b33b22ce7
Atanas Chanev <atanas.chanev@bitrise.io>
2022-03-23 14:25:27 +0000

    Set config variables

--- step.swift
+++ step.swift
@@ -2,14 +2,24 @@
 
 import Foundation
 
+let filePath = ProcessInfo.processInfo.environment["file_path"] ?? ""
+let appSlug = ProcessInfo.processInfo.environment["app_slug"] ?? ""
+let buildSlug = ProcessInfo.processInfo.environment["build_slug"] ?? ""
+let wasabiAccessKey = ProcessInfo.processInfo.environment["aws_access_key"] ?? ""
+let wasabiSecretKey = ProcessInfo.processInfo.environment["aws_secret_key"] ?? ""
+let bucketName = ProcessInfo.processInfo.environment["bucket_name"] ?? ""
+let bucketRegion = ProcessInfo.processInfo.environment["bucket_region"] ?? ""
+let pathInBucket = ProcessInfo.processInfo.environment["path_in_bucket"] ?? ""
+let fileAccessLevel = ProcessInfo.processInfo.environment["file_access_level"] ?? ""
+
 // Print options
 print("Configs:")
-print("* file_path: \(ProcessInfo.processInfo.environment["file_path"] ?? "")")
-print("* app_slug: \(ProcessInfo.processInfo.environment["app_slug"] ?? "")")
-print("* build_slug: \(ProcessInfo.processInfo.environment["build_slug"] ?? "")")
-print("* aws_access_key: \((ProcessInfo.processInfo.environment["aws_access_key"] != nil) ? "***" : "")")
-print("* aws_secret_key: \((ProcessInfo.processInfo.environment["aws_secret_key"] != nil) ? "***" : "")")
-print("* bucket_name: \(ProcessInfo.processInfo.environment["bucket_name"] ?? "")")
-print("* bucket_region: \(ProcessInfo.processInfo.environment["bucket_region"] ?? "")")
-print("* path_in_bucket: \(ProcessInfo.processInfo.environment["path_in_bucket"] ?? "")")
-print("* file_access_level: \(ProcessInfo.processInfo.environment["file_access_level"] ?? "")")
+print("* file_path: \(filePath)")
+print("* app_slug: \(appSlug)")
+print("* build_slug: \(buildSlug)")
+print("* aws_access_key: \(wasabiAccessKey.isEmpty ? "" : "***")")
+print("* aws_secret_key: \(wasabiSecretKey.isEmpty ? "" : "***")")
+print("* bucket_name: \(bucketName)")
+print("* bucket_region: \(bucketRegion)")
+print("* path_in_bucket: \(pathInBucket)")
+print("* file_access_level: \(fileAccessLevel)")
