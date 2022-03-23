a0be6ae1bb97d1aa4fd159845590c5552702cc2e
Atanas Chanev <atanas.chanev@bitrise.io>
2022-03-23 13:59:19 +0000

    Added all input parameters

--- step.swift
+++ step.swift
@@ -5,3 +5,11 @@ import Foundation
 // Print options
 print("Configs:")
 print("* file_path: \(ProcessInfo.processInfo.environment["file_path"] ?? "")")
+print("* app_slug: \(ProcessInfo.processInfo.environment["app_slug"] ?? "")")
+print("* build_slug: \(ProcessInfo.processInfo.environment["build_slug"] ?? "")")
+print("* aws_access_key: \((ProcessInfo.processInfo.environment["aws_access_key"] != nil) ? "***" : "")")
+print("* aws_secret_key: \((ProcessInfo.processInfo.environment["aws_secret_key"] != nil) ? "***" : "")")
+print("* bucket_name: \(ProcessInfo.processInfo.environment["bucket_name"] ?? "")")
+print("* bucket_region: \(ProcessInfo.processInfo.environment["bucket_region"] ?? "")")
+print("* path_in_bucket: \(ProcessInfo.processInfo.environment["path_in_bucket"] ?? "")")
+print("* file_access_level: \(ProcessInfo.processInfo.environment["file_access_level"] ?? "")")
